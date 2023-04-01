680ea0ede85a9a283d4b95c30a666e9d8d5e47c6
sunil9010 <tsn9010@gmail.com>
2023-04-01 12:42:54 +0000

    typed_solution

---
+++ src/components/TimelineView/index.js
@@ -0,0 +1,25 @@
+// Write your code here
+import {Chrono} from 'react-chrono'
+import CourseTimeLineCard from '../CourseTimelineCard'
+import ProjectTimeLineCard from '../ProjectTimelineCard'
+import './index.css'
+
+const TimeLineView = props => {
+  const {timelineItemsList} = props
+  return (
+    <div className="mainBg">
+      <h1>MY JOURNEY OF CCBP 4.0</h1>
+      <div className="chronoDiv">
+        <Chrono mode="VERTICAL" items={timelineItemsList}>
+          {timelineItemsList.map(each => {
+            if (each.categoryId === 'COURSE') {
+              return <CourseTimeLineCard data={each} />
+            }
+            return <ProjectTimeLineCard data={each} />
+          })}
+        </Chrono>
+      </div>
+    </div>
+  )
+}
+export default TimeLineView
